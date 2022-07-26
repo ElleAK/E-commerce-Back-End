b3392461b402675cee82a1a6f904c49b352e3095
Lindsey Kelly <nightbirdphoto@gmail.com>
2022-07-25 20:02:17 -0400

    find All route for category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,8 +6,16 @@ const { Category, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
-  Category.findAll()
-  .then(dbCatergorytData => res.json(dbCatergoryData))
+  Category.findAll({
+    attributes: ["id", "category_name"],
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"],
+      },
+    ],
+  })
+  .then(dbCategoryData => res.json(dbCategoryData))
   .catch(err => {
     console.log(err);
     res.status(500).json(err);
